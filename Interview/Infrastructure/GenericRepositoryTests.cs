439b45b60e559cfa76b5b11157931db24d586839
kamilRepo <kamilpawlusinski@gmail.com>
2019-04-23 19:58:09 +0100

    Implemented Generic Repository Tests

--- Interview/Infrastructure/GenericRepositoryTests.cs
+++ Interview/Infrastructure/GenericRepositoryTests.cs
@@ -9,8 +9,238 @@ namespace Interview.Infrastructure
     public class GenericRepositoryTests
     {
         [TestMethod]
-        public void MethodName_StateUnderTest_ExpectedBehavior()
+        public void FindById_ContainId_FindItem()
         {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            var item = repository.FindById(1);
+
+            //Assertions
+            Assert.AreEqual(storeableA.Name, item.Name);
+        }
+
+        [TestMethod]
+        public void FindById_DontContainId_CantFindItem()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            var item = repository.FindById(3);
+
+            //Assertions
+            Assert.IsNull(item);
+        }
+
+        [TestMethod]
+        public void Save_ContainId_UpdatedExistItem()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 1,
+                Name = "b"
+            };
+
+            //Action
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Assertions
+            var items = repository.All();
+            var item = repository.FindById(1);
+
+            Assert.AreEqual(1, items.Count());
+            Assert.AreEqual(storeableB.Name, item.Name);
+        }
+
+        [TestMethod]
+        public void Save_DontContainId_AddedNewItems()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            //Action
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Assertions
+            var items = repository.All();
+            var itemA = repository.FindById(1);
+            var itemB = repository.FindById(2);
+
+            Assert.AreEqual(2, items.Count());
+            Assert.AreEqual(storeableA.Name, itemA.Name);
+            Assert.AreEqual(storeableB.Name, itemB.Name);
+        }
+
+        public void Save_CheckReturnInstance_CorrectInstance()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            var itemA = repository.FindById(1);
+
+            //Assertions
+            Assert.IsInstanceOfType(itemA, typeof(Storeable));
+        }
+
+        [TestMethod]
+        public void Delete_DontContainId_NoAction()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            repository.Delete(3);
+
+            //Assertions
+            var items = repository.All();
+            var itemA = repository.FindById(1);
+            var itemB = repository.FindById(2);
+            var itemC = repository.FindById(3);
+
+            Assert.AreEqual(2, items.Count());
+            Assert.AreEqual(storeableA.Name, itemA.Name);
+            Assert.AreEqual(storeableB.Name, itemB.Name);
+            Assert.IsNull(itemC);
+        }
+
+        [TestMethod]
+        public void Delete_ContainId_RemovedItem()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            repository.Delete(2);
+
+            //Assertions
+            var items = repository.All();
+            var itemA = repository.FindById(1);
+            var itemB = repository.FindById(2);
+
+            Assert.AreEqual(1, items.Count());
+            Assert.AreEqual(storeableA.Name, itemA.Name);
+            Assert.IsNull(itemB);
+        }
+
+        [TestMethod]
+        public void All_ReturnAll_AllReturned()
+        {
+            //Preparation
+            IRepository<Storeable> repository = new GenericRepository<Storeable>();
+
+            Storeable storeableA = new Storeable()
+            {
+                Id = 1,
+                Name = "a"
+            };
+            Storeable storeableB = new Storeable()
+            {
+                Id = 2,
+                Name = "b"
+            };
+
+            repository.Save(storeableA);
+            repository.Save(storeableB);
+
+            //Action
+            var items = repository.All();
+
+            //Assertions
+            Assert.AreEqual(2, items.Count());
         }
     }
 }
